11ae5da49577772a6c78c14e6469ae510e79c6a6
Yuya Chiba <cy.blue.9@gmail.com>
2019-03-09 12:19:16 +0900

    Fix a product data

--- _site/js/toggle_all_hover.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.all_hover_toggle').on('click', function () {
-        var cache = $('#portfolio-work .block .portfolio-contant ul li');
-        $(cache).each(function () {
-            $(this).toggleClass('hover');
-        });
-    });
-})
